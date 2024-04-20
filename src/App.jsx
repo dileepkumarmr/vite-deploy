e5942ad1e004413eb7d44197da6c3a908d890c59
dileepkumarmr <dileepmr35@gmail.com>
2024-04-20 12:04:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function App() {
+
+
+  return (
+    
+<div className="underline font-bold bg-slate-950 text-white">
+DIleep</div>
+
+  )
+}
+
+export default App
